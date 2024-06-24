10e77dc2de60ff4cc0e6eef043da620273f64b08
CETECOM <CETECOM@AVAWSV02LC0807.duoc.acad>
2024-06-24 11:30:59 -0400

    avance_clase

--- TrabajoSemestral/Bookstore/urls.py
+++ TrabajoSemestral/Bookstore/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from GameStore import views
 
 urlpatterns = [
-    
+    path("",views.PaginaInicial, name="PaginaInicial"),
 ]
--- TrabajoSemestral/Bookstore/views.py
+++ TrabajoSemestral/Bookstore/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def PaginaInicial(request):
+    context = {
+        "user": "",
+    }
+    return render(request, "PaginaInicial.html", context)
