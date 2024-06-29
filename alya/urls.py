9255217b3739f6bf2d55e6624c6c5750789ddb99
Vich0yarz <133251252+Vich0yarz@users.noreply.github.com>
2024-06-29 12:12:06 -0400

    migration
    Migración a django completa del proyecto

---
+++ alya/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('inicio', views.index, name='index'),
+    path('nuevo', views.mostnew, name='nuevo'),
+    path('escritores', views.writers, name='escritores'),
+    path('generos', views.generos, name='generos'),
+    path('escritor', views.escritor, name='escritor'),
+    path('formulario', views.formulario, name='formulario')
+]
