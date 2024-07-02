b2a6e7ac78acd5a91c9bb7a13520b0fc91261633
Vich0yarz <133251252+Vich0yarz@users.noreply.github.com>
2024-07-02 18:12:00 -0400

    book/author

--- alya/models.py
+++ alya/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
-    rut = models.CharField(primary_key=True, max_length=10)
+    rut = models.CharField(primary_key=True, max_length=12)
     nombre = models.CharField(max_length=20)
     apellido_paterno = models.CharField(max_length=20)
     apellido_materno = models.CharField(max_length=20)
@@ -20,3 +19,25 @@ class User(models.Model):
             + " "
             + str(self.apellido_materno)
         )    
+
+class Author(models.Model):
+    id = models.CharField(primary_key=True, max_length=10)
+    name = models.CharField(max_length=20)
+    born_date = models.DateField()
+    picture = models.CharField(max_length=10)
+
+    def __str__(self):
+        return (str(self.name))
+    
+class Book(models.Model):
+    id = models.CharField(primary_key=True, max_length=10)
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=20)
+    year = models.IntegerField()
+    synopsis = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    available = models.BooleanField()
+    picture = models.CharField(max_length=10)
+
+    def __str__(self):
+        return (str(self.title))
